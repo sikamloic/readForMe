41b8b3fc98754746dd6f0bc8a1cc8cea7cdf65d8
unknown <sikamloic>
2023-11-27 17:30:40 +0100

    27/11/2023

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    SERVER_URL: "http://localhost:3000",
-    // SERVER_URL: "https://read-for-me-api.vercel.app",
+    // SERVER_URL: "http://localhost:3000",
+    SERVER_URL: "https://read-for-me-api.vercel.app",
     production: true
   };
